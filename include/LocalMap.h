d863e2ef0d8303845abb9cee76ed84cf630fe71f
swagquotient0 <54880010+swagquotient0@users.noreply.github.com>
2019-11-25 16:07:36 -0500

    Update LocalMap.h
    Upating LocalMap.h correcting cpplint errors

--- include/LocalMap.h
+++ include/LocalMap.h
@@ -3,22 +3,21 @@
  Copyright 2019 Aman Virmani, Sri Sai Kaushik, Sandra Tinta
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 /*
- * @file ButlerDemo.hpp
+ * @file LocalMap.h
  * @driver         Aman Virmani
  * @navigator      Sandra Tinta 
  * @design keeper  Mushty Sri Sai Kaushik
  * Created on 24 November 2019
  * @brief LocalMap header file
  */
+#ifndef INCLUDE_LOCALMAP_H_
 
-#include<iostream>
 #include<ros/ros.h>
+#include<iostream>
 
 class LocalMap {
-
-public:
+ public:
   /**
    * @brief Variable that defines the current location of        the bot.
    */
@@ -41,5 +40,4 @@ public:
    * @return none
    */
   void getLocalMap():nav_msgs::OccupancyGrid;
-
-}
+};
