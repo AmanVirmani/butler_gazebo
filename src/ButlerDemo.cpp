d79e1f2952b6a258f307fc276fe837a69e44f87b
stinta <stinta@gmail.com>
2019-12-08 16:19:13 -0500

    Modify the demo driver class to handle order requests and setting robot back to the home location

--- src/ButlerDemo.cpp
+++ src/ButlerDemo.cpp
@@ -2,21 +2,13 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include "Navigation.h"
-
+#include "WarehouseLocation.h"
+#include "LocalMap.h"
+#include "ButlerOrder.h"
 
 /** function declarations **/
 //bool moveToGoal(double xGoal, double yGoal);
 char choose();
-
-/** declare the coordinates of interest **/
-double xCafe = -0.16;
-double yCafe = -1.52;
-double xOffice1 = -1.1 ;
-double yOffice1 = 2.0;
-double xOffice2 = 0.1 ;
-double yOffice2 = 0;
-double xOffice3 = 3.0 ;
-double yOffice3 = 0;
 
 bool goalReached = false;
 
@@ -25,10 +17,34 @@ int main(int argc, char** argv){
    auto butler_nav = Navigation();
    butler_nav.init(argc,argv);
 
-   char choice = 'q';
-   do{
-      choice =choose();
-      if (choice == '0'){
+//----- TEST-
+  auto wmap = LocalMap();
+  wmap.initLocalMap();
+  auto myorder = ButlerOrder();
+  auto homeLoc = wmap.getLoc('o');
+//-----
+ 
+ goalReached = butler_nav.isDestinationReached(homeLoc.getX(), homeLoc.getY());
+  char choice = 'q';
+
+do{   
+
+
+     choice =choose();
+     if (choice == 'o') {
+
+
+     auto s= myorder.getOrder(wmap);
+     auto from = s.first;
+     auto to = s.second;
+
+      goalReached = butler_nav.isDestinationReached(from.getX(), from.getY());
+      ros::spinOnce();
+      goalReached = butler_nav.isDestinationReached(to.getX(), to.getY());
+      ros::spinOnce();
+      goalReached = butler_nav.isDestinationReached(homeLoc.getX(), homeLoc.getY());
+      ros::spinOnce();
+      /*if (choice == '0'){
          goalReached = butler_nav.isDestinationReached(xCafe, yCafe);
       }else if (choice == '1'){
          goalReached = butler_nav.isDestinationReached(xOffice1, yOffice1);
@@ -36,8 +52,8 @@ int main(int argc, char** argv){
          goalReached = butler_nav.isDestinationReached(xOffice2, yOffice2);
       }else if (choice == '3'){
          goalReached = butler_nav.isDestinationReached(xOffice3, yOffice3);
-      }
-      if (choice!='q'){
+      }*/
+      if (choice !='q'){
          if (goalReached){
             ROS_INFO("Congratulations!");
             ros::spinOnce();
@@ -46,21 +62,16 @@ int main(int argc, char** argv){
             ROS_INFO("Hard Luck!");
          }
       }
-   }while(choice !='q');
+}   }while(choice !='q');
    return 0;
 }
 
 char choose(){
 	char choice='q';
-	std::cout<<"|-------------------------------|"<<std::endl;
-	std::cout<<"|PRESSE A KEY:"<<std::endl;
-	std::cout<<"|'0': Cafe "<<std::endl;
-	std::cout<<"|'1': Office 1 "<<std::endl;
-	std::cout<<"|'2': Office 2 "<<std::endl;
-	std::cout<<"|'3': Office 3 "<<std::endl;
+	std::cout<<"|'o': Enter Order "<<std::endl;
 	std::cout<<"|'q': Quit "<<std::endl;
 	std::cout<<"|-------------------------------|"<<std::endl;
-	std::cout<<"|WHERE TO GO?";
+	std::cout<<"|WHAT TO DO?";
 	std::cin>>choice;
 
 	return choice;
