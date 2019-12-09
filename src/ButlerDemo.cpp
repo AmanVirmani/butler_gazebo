e22d90499402c952400502317b96d2926fa54604
stinta <stinta@gmail.com>
2019-12-08 23:06:52 -0500

    [Task 3.19] Updated file to handle cpplint corrections

--- src/ButlerDemo.cpp
+++ src/ButlerDemo.cpp
@@ -1,3 +1,18 @@
+/*
+ The BSD-3-Clause License
+ Copyright 2019 Aman Virmani, Sri Sai Kaushik, Sandra Tinta
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*
+ * @file ButlerDemo.cpp
+ * @driver         Aman Virmani
+ * @navigator      Sandra Tinta 
+ * @design keeper  Mushty Sri Sai Kaushik
+ * Created on 24 November 2019
+ * @brief This Class is the demoDriver it instatiates all object location for map
+ *      for navigatoin and for the orderd 
+ *    
+ */
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
@@ -7,74 +22,63 @@
 #include "ButlerOrder.h"
 
 /** function declarations **/
-//bool moveToGoal(double xGoal, double yGoal);
 char choose();
-
 bool goalReached = false;
 
-/* TODO THIS IS ONLY A TEMPORARY HELPER MAIN BASED ON TUTORIALS*/
-int main(int argc, char** argv){
-   auto butler_nav = Navigation();
-   butler_nav.init(argc,argv);
+int main(int argc, char** argv) {
+  // Initilizing Navigation Object
+  auto butler_nav = Navigation();
+  butler_nav.init(argc , argv);
 
-//----- TEST-
+  // initializing LocalMap which holds stattions locations
   auto wmap = LocalMap();
   wmap.initLocalMap();
+  // Order object which returs a pair of locatoins
   auto myorder = ButlerOrder();
+  // setting and retrieving home loaction
   auto homeLoc = wmap.getLoc('o');
-//-----
- 
- goalReached = butler_nav.isDestinationReached(homeLoc.getX(), homeLoc.getY());
+  // setting initial locaiton to the homeStation
+  goalReached = butler_nav.isDestinationReached(homeLoc.getX(), homeLoc.getY());
   char choice = 'q';
 
-do{   
-
-
-     choice =choose();
+  do {
+     choice = choose();
      if (choice == 'o') {
+       // prints menu for user to entry from and to station
+       auto s = myorder.getOrder(wmap);
+       // retrieving coordintes
+       auto from = s.first;
+       auto to = s.second;
+       // navigation to from location
+       goalReached = butler_nav.isDestinationReached(from.getX(), from.getY());
+       ros::spinOnce();
+       // navigaring to to locatopm
+       goalReached = butler_nav.isDestinationReached(to.getX(), to.getY());
+       ros::spinOnce();
+       // navigating to home location
+       goalReached = butler_nav.isDestinationReached(
+                           homeLoc.getX(), homeLoc.getY());
+       ros::spinOnce();
 
-
-     auto s= myorder.getOrder(wmap);
-     auto from = s.first;
-     auto to = s.second;
-
-      goalReached = butler_nav.isDestinationReached(from.getX(), from.getY());
-      ros::spinOnce();
-      goalReached = butler_nav.isDestinationReached(to.getX(), to.getY());
-      ros::spinOnce();
-      goalReached = butler_nav.isDestinationReached(homeLoc.getX(), homeLoc.getY());
-      ros::spinOnce();
-      /*if (choice == '0'){
-         goalReached = butler_nav.isDestinationReached(xCafe, yCafe);
-      }else if (choice == '1'){
-         goalReached = butler_nav.isDestinationReached(xOffice1, yOffice1);
-      }else if (choice == '2'){
-         goalReached = butler_nav.isDestinationReached(xOffice2, yOffice2);
-      }else if (choice == '3'){
-         goalReached = butler_nav.isDestinationReached(xOffice3, yOffice3);
-      }*/
-      if (choice !='q'){
-         if (goalReached){
+       if (choice !='q') {
+         if (goalReached) {
             ROS_INFO("Congratulations!");
             ros::spinOnce();
 
-         }else{
+         } else {
             ROS_INFO("Hard Luck!");
          }
       }
-}   }while(choice !='q');
-   return 0;
+     }   }while(choice !='q');
+  return 0;
 }
 
-char choose(){
-	char choice='q';
-	std::cout<<"|'o': Enter Order "<<std::endl;
-	std::cout<<"|'q': Quit "<<std::endl;
-	std::cout<<"|-------------------------------|"<<std::endl;
-	std::cout<<"|WHAT TO DO?";
-	std::cin>>choice;
-
-	return choice;
-
-
+char choose() {
+  char choice = 'q';
+  std::cout << "|'o': Enter Order " << std::endl;
+  std::cout << "|'q': Quit "<< std::endl;
+  std::cout << "|-------------------------------|" << std::endl;
+  std::cout << "|WHAT TO DO?";
+  std::cin >> choice;
+  return choice;
 }
